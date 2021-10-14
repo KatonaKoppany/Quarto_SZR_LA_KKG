d6412aca57aedf18de78a9bc37681519c5becb35
KatonaKoppany <hypervenom129@gmail.com>
2021-10-14 11:30:12 +0200

    Kezdés form alapok

--- Quarto_SZR_LA_KKG/Form1.Designer.cs
+++ Quarto_SZR_LA_KKG/Form1.Designer.cs
@@ -29,20 +29,85 @@ namespace Quarto_SZR_LA_KKG
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.jatekos1_TBOX = new System.Windows.Forms.TextBox();
+            this.jatekos2_TBOX = new System.Windows.Forms.TextBox();
+            this.start_BTN = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Játékos 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Játékos 2:";
+            // 
+            // jatekos1_TBOX
+            // 
+            this.jatekos1_TBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.jatekos1_TBOX.Location = new System.Drawing.Point(105, 33);
+            this.jatekos1_TBOX.Name = "jatekos1_TBOX";
+            this.jatekos1_TBOX.Size = new System.Drawing.Size(100, 24);
+            this.jatekos1_TBOX.TabIndex = 2;
+            // 
+            // jatekos2_TBOX
+            // 
+            this.jatekos2_TBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.jatekos2_TBOX.Location = new System.Drawing.Point(105, 93);
+            this.jatekos2_TBOX.Name = "jatekos2_TBOX";
+            this.jatekos2_TBOX.Size = new System.Drawing.Size(100, 24);
+            this.jatekos2_TBOX.TabIndex = 3;
+            // 
+            // start_BTN
+            // 
+            this.start_BTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.start_BTN.Location = new System.Drawing.Point(90, 163);
+            this.start_BTN.Name = "start_BTN";
+            this.start_BTN.Size = new System.Drawing.Size(100, 35);
+            this.start_BTN.TabIndex = 4;
+            this.start_BTN.Text = "Start";
+            this.start_BTN.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.start_BTN);
+            this.Controls.Add(this.jatekos2_TBOX);
+            this.Controls.Add(this.jatekos1_TBOX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(300, 300);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
             this.Name = "Form1";
             this.Text = "Quarto!";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox jatekos1_TBOX;
+        private System.Windows.Forms.TextBox jatekos2_TBOX;
+        private System.Windows.Forms.Button start_BTN;
     }
 }
 
